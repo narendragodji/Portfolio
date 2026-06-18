708d01b21e6767d6e4719260038d1ea9acba761d
Akash Singh (MU SIGMA BUSINESS SOLUTIONS LLC) <v-asingh2@microsoft.com>
2026-06-18 21:54:42 +0530

    Updated Libraries and layouts

---
+++ frontend/src/components/hero-section/herosection.tsx
@@ -0,0 +1,12 @@
+export default function HeroSection() {
+    return(
+        <section className="min-h-screen flex py-3 px-6">
+            <div className ="flex justify-center  w-full">
+            <div className = "max-w-4xl text-center">
+                <h1 className ="text-6xl font-bold mb-4">TexTual Header
+                </h1>
+            </div>
+            </div>
+        </section>
+    );
+}
